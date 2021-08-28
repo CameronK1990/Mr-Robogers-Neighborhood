1e9337dd104e9e4ceebb8399c1fbbd1057ada393
Cameron Kronsbein <cameron.kronsbein@gmail.com>
2021-08-28 15:07:59 -0700

    ADD corrected business logic for numerical priority

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@ function robotResponse (sentence) {
   let robotResArr = [];
   for (let i = 0; i < sentence.length; i++) {
     let character = sentence.charAt(i);
-    if (character.includes("1")) {
-      robotResArr.push("Beep!");
+    if (character.includes("3")) {
+      robotResArr.push("Will you be my neighbor?");
     } else if (character.includes("2")) {
       robotResArr.push("Boop!");
-    } else if (character.includes("3")) {
-      robotResArr.push("Will you be my neighbor");
+    } else if (character.includes("1")) {
+      robotResArr.push("Beep!");
     } else {
       robotResArr.push(character);
     }
